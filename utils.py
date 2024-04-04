0ed4fccf3f99eb4aa1c499a56fa10d5091297257
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2024-04-04 19:49:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,66 +1,56 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-    MessagesPlaceholder
-)
-
+from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
 import streamlit as st
-from streamlit_chat import message
-from utils import *
 
-st.title("Personal AI Bot")
+openai.api_key = "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" ## find at platform.openai.com
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
-if 'responses' not in st.session_state:
-    st.session_state['responses'] = ["How can I assist you?"]
+pinecone.init(api_key='d230654b-0530-4109-a7f8-d6d83d952e62',
+              environment='gcp-starter' 
+             )
 
-if 'requests' not in st.session_state:
-    st.session_state['requests'] = []
+index = pinecone.Index('chatbot')
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", 
-openai_api_key="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") 
+def find_match(input):
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=2, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
 
-if 'buffer_memory' not in st.session_state:
-            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+def query_refiner(conversation, query):
 
+    response = openai.Completion.create(
+    model="text-embedding-ada-002",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+#     response = client.chat.completions.create(
+#         model = "gpt-3.5-turbo",
+#         # prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG:
+#         # \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+#         messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Who won the world series in 2020?"},
+#     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#     {"role": "user", "content": "Where was it played?"}
+#   ],
+#         temperature=0.3,
+#         max_tokens=256,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#     )
+#     return response['choices'][0]
 
-system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
-and if the answer is not contained within the text below, say 'I don't know'""")
-
-
-human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
-
-prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
-
-conversation = ConversationChain(llm=llm, memory=st.session_state.buffer_memory, verbose=True, prompt=prompt_template)
-
-# container for chat history
-response_container = st.container()
-# container for text box
-textcontainer = st.container()
-
-
-with textcontainer:
-    query = st.text_input("Query: ", key="input")
-    if query:
-        with st.spinner("typing..."):
-            conversation_string = get_conversation_string()
-            # st.code(conversation_string)
-            refined_query = query_refiner(conversation_string, query)
-            st.subheader("Refined Query:")
-            st.write(refined_query)
-            context = find_match(refined_query)
-            # print(context)  
-            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-        st.session_state.requests.append(query)
-        st.session_state.responses.append(response) 
-with response_container:
-    if st.session_state['responses']:
-
-        for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i],key=str(i))
-            if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
